efea1e4ad14200cbdb79b562bfc3b7af1eecd569
Hady Ashraf <hady.ashraf.163@gmail.com>
2023-12-21 13:46:22 +0200

    Add project files.

---
+++ ShoppingCart.DataAccess/Model/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Model
+{
+    // To establish the many-to-many relationship between (Product & Category)!
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}
